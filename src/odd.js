77c8eccd7f2796cf9cdec00d026df1c6a45a6876
Ethan Charles <ECharles1@ucvts.tec.nj.us>
2020-01-30 13:14:48 -0500

    finished odd.js

---
+++ src/odd.js
@@ -0,0 +1,40 @@
+const readlineSync = require("readline-sync");
+
+const MIN = 1;
+const MAX = Number.MAX_SAFE_INTEGER;
+
+let positiveInteger;
+
+do {
+        positiveInteger = Number(readlineSync.question("\nPositive integer: "));
+
+} while (positiveInteger > MAX
+        || positiveInteger < MIN
+        || Number.isNaN(positiveInteger)
+        || Number.isInteger(positiveInteger) == false)
+
+let output = 0;
+let x = 0;
+
+while (positiveInteger > 0) {
+        while ((positiveInteger % 10) !== 0) {
+                positiveInteger -= 1;
+                x += 1;
+        }
+
+        x = Number(x);
+        output = Number(output);
+
+        if (x % 2 === 1) {
+                output += x;
+        }
+
+        positiveInteger /= 10;
+
+        x = 0;
+
+}
+
+output = output + ".";
+
+console.log(output);
