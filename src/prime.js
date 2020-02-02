b1fb8fbc3e6fa4b1ca975e13d6cfc2b966a9d463
Ethan Charles <bigetc1723@gmail.com>
2020-02-02 16:30:34 -0500

    finished prime.js

---
+++ src/prime.js
@@ -0,0 +1,28 @@
+const readlineSync = require("readline-sync");
+
+const MIN = 0;
+const MAX = Number.MAX_SAFE_INTEGER;
+
+let nonNegativeInteger;
+
+do {
+        nonNegativeInteger = Number(readlineSync.question("\nNon-negative integer: "));
+} while (nonNegativeInteger > MAX
+        || nonNegativeInteger < MIN
+        || Number.isNaN(nonNegativeInteger) == true
+        || Number.isInteger(nonNegativeInteger) == false);
+
+if ((nonNegativeInteger == 0) || (nonNegativeInteger == 1)) {
+        console.log("Not prime.");
+} else if (nonNegativeInteger == 2) {
+        console.log("Prime.");
+} else {
+        for (var i = 2; i < nonNegativeInteger; i++) {
+                if (Number.isInteger(nonNegativeInteger / i) == true) {
+                        console.log("Not prime.");
+                        break;
+                } else if ((Number.isInteger(nonNegativeInteger / i) == false) && (Number(i) + 1 == nonNegativeInteger)) {
+                        console.log("Prime.");
+                }
+        }
+}
